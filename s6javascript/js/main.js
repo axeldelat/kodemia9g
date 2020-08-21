b5f6d6c054c210e5ade4c801ee1fa94a3f40891e
Axel de la Torre <axeldelat@gmail.com>
2020-08-20 21:13:28 -0500

    Empezamos JS

---
+++ s6javascript/js/main.js
@@ -0,0 +1,63 @@
+/*
+var valor1 = parseInt(prompt("ingrese primer dato o número:"))
+var valor2 = parseInt(prompt("ingrese segundo dato o número"))
+
+var addition = valor1 + valor2
+var substraction = valor1 - valor2
+var multiplication = valor1*valor2
+var division = valor1 / valor2
+
+console.log (addition)
+console.log (substraction)
+console.log (multiplication)
+console.log (division)
+
+
+document.body.innerHTML = "<h1>" +  addition +"</h1>" + "<h1>" +  substraction +"</h1>" + "<h1>" +  multiplication +"</h1>" + "<h1>" +  division +"</h1>"; //Comentario en línea
+Comentario largo */
+
+
+/*
+var valor1 = parseInt(prompt("ingrese primer dato o número:"))
+var valor2 = parseInt(prompt("ingrese segundo dato o número"))
+
+var divisionValor1 = valor1%2
+
+var divisionValor2 = valor2%2
+
+if (divisionValor1 === 0 && divisionValor1 === 0){
+    console.log("El primer y segundo valor son PARES")
+}   else if (divisionValor1 === 1 && divisionValor1 === 1) {
+    console.log("El primer y segundo valor son IMPARES")
+}   else if (divisionValor1 === 0){
+    console.log("El primer valor es PAR, pero el segundo no.")
+}   else {
+    console.log("El segundo valor es PAR, pero el primero no.")
+}
+ */
+
+
+
+/*
+ // Preguntar al usuario cual es la cpital de México
+ //Verificar si es correcto
+ // imprimir "mensaje de correcto"
+ //si no
+ //imprimir mensaje de error
+
+let capital = prompt('¿Cuál es la capital de México?')
+let capitalReset = capital.toLowerCase().trim()
+
+if(capitalReset === 'ciudad de méxico') {
+    console.log('¡Es correcto!')
+} else if(capitalReset === 'ciudad de mexico') {
+    console.log('La respuesta es correcta, pero recuerda poner acento en la e a México. La respuesta correcta es: Ciudad de México.')
+} else if(capitalReset === 'cdmx') {
+    console.log('Si pero no, la respuesta correcta es: Ciudad de México')
+} else {
+    console.log('La resuesta es incorrecta, la respuesta correcta es: Ciudad de México')
+}
+
+*/
+
+
